cb8e74d6aa85e2bdd91955bc2a5e43eb435529cc
mariegoltermann <mari09a7@stud.kea.dk>
2022-10-06 14:00:19 +0200

    fjds

--- src/main.js
+++ src/main.js
@@ -1,18 +1,2 @@
 import "./style.css";
 
-
-const acceptCookie = document.querySelector("#accepter");
-const denyCookie = document.querySelector("#afvis");
-const cookieboks = document.querySelector("#cookieboks");
-
-acceptCookie.addEventListener("click", fjernCookieBoks);
-denyCookie.addEventListener("click", fjernCookieBoks);
-
-function fjernCookieBoks() {
-    cookieboks.classList.add("hidden");
-    sessionStorage-SVGTextPositioningElement("fjerncookieboks", true);
-}
-
-if (sessionStorage.getItem("fjerncookieboks")){
-    cookieboks.classList.add("hidden");
-}
