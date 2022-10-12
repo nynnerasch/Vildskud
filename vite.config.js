f4b39bf5669045005d52ab40aa86a02449c268c4
mariegoltermann <mari09a7@stud.kea.dk>
2022-10-12 09:38:10 +0200

    sa

--- vite.config.js
+++ vite.config.js
@@ -14,16 +14,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-<<<<<<< HEAD
         events: resolve(__dirname, "src/events.html"),
         vaermed: resolve(__dirname, "src/vaermed.html"),
         festivalen: resolve(__dirname, "src/Festivalen.html"),
         kontakt: resolve(__dirname, "src/Kontakt.html"),
         forestillinger: resolve(__dirname, "src/forestillinger.html"),
-=======
-        vaermed: resolve(__dirname, "src/vaermed.html"),
-
->>>>>>> 236bf81bd7101912b3e9d44e61bf4bdadbd7b8fd
         // about: resolve(__dirname, "src/about.html"),
       },
     },
